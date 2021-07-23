79ca09e1617b6ae551363e269f9bbfc38992adc9
Saurab-Rakshit <69044807+Saurab-Rakshit@users.noreply.github.com>
2021-07-23 18:15:21 +0530

    Add files via upload

---
+++ code.js
@@ -0,0 +1,56 @@
+function validation(){
+    var user = document.getElementById('user').value;
+    var email = document.getElementById('email').value;
+    var mobno = document.getElementById('mobno').value;
+    var pass = document.getElementById('pass').value;
+    var cpass = document.getElementById('cpass').value;
+
+    //* Username
+    if (user === '') {
+        document.getElementById('username').innerHTML = '*Please fill user';
+        return false;            
+    }else if (user.length <3  || user.length > 20) {
+        document.getElementById('username').innerHTML = '*username must be between 3 to 10';
+        return false;            
+    }else if(!isNaN(user)){ //TODO: !isNaN(user) For Avoiding Numbers in Username Input
+        document.getElementById('username').innerHTML = '*Numbers Not Allowed'
+        return false;
+    }    
+    //* Email
+    if (email === '') {
+        document.getElementById('emailid').innerHTML  = '*Please fill email';
+        return false;            
+    }
+    //* Mobile NO
+    if (mobno === '') {
+        document.getElementById('mobileno').innerHTML = '*Please fill mobile no';
+        return false;            
+    }else if(isNaN(mobno)){  //TODO: isNaN(mobno) For avoiding Alphabets in the Mobile Number input
+        document.getElementById('mobileno').innerHTML = '* Alphabets not allowed';
+        return false;
+    }else if (mobno.length != 10) {
+        document.getElementById('mobileno').innerHTML = '*Mobile No. must be of 10 digits';
+        return false;            
+    }  
+
+    //* Password
+    if (pass === '') {
+        document.getElementById('password').innerHTML = '*Please fill password';
+        return false;            
+    }else if(!pass.length > 5 ){
+        document.getElementById('password').innerHTML = '*Password length must be greater than 5'
+        return false;
+    }
+    //* confirming password
+    if(pass != cpass){
+        document.getElementById('cpassword').innerHTML = '*Password Not matching';
+        return false;
+    }
+
+    //* Confirm Password
+    if (cpass === '') {
+        document.getElementById('cpassword').innerHTML = '*Please fill C Password';
+        return false;            
+    }    
+    
+}
